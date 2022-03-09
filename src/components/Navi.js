b60866ffda21aa561356fcc96f0356350b6366dd
Marius <mulamamarius@gmail.com>
2022-03-10 01:24:56 +0300

    Added user avatar, name and logout on the navbar plus styling

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,25 +1,53 @@
 import React from "react";
+import "../App.css";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Button } from "react-bootstrap";
+import avatar from "../images/avatar.png";
 
-function Navi() {
+function Navi({ user }) {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="mb-1">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        className="mb-1"
+      >
         <Link to="/">
           <Navbar.Brand>LOGO</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-          </Nav>
+          <Nav className="mr-auto"></Nav>
           <Nav className="d-flex justify-content-between mr-4">
-            <Link to="/login">
-              <Button variant="outline-info" >
-              LOGIN
-              </Button>
+            {user ? (
+              <>
+              <Link to="/profile">
+                <Navbar.Brand>
+                  <img
+                    src={avatar}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top user-avatar"
+                    alt=""
+                  />
+                  {user}
+                </Navbar.Brand>
+              </Link>
+
+              <Link to="/login">
+                <Button variant="outline-info">LOGOUT</Button>
+              </Link>
+
+              </>
+
               
-            </Link>
+            ) : (
+              <Link to="/login">
+                <Button variant="outline-info">LOGIN</Button>
+              </Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
