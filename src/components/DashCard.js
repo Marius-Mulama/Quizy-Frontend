e35269ecab8c944321a04f3d2e9561efc32d95c5
Marius <mulamamarius@gmail.com>
2022-03-10 01:38:40 +0300

    TO be implemented

---
+++ src/components/DashCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import {Card} from "react-bootstrap";
+
+function DashCard() {
+  return (
+    <div>
+      <Card>
+        
+      </Card>
+      
+    </div>
+  )
+}
+
+export default DashCard
