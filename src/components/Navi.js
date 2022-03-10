aaac707ba0ccd6b68276c1c5275de6998af03e96
Marius <mulamamarius@gmail.com>
2022-03-11 01:03:07 +0300

    Revert "Added listeners to the different social logins --- Not yet completed--"
    This reverts commit ee4b34d8ece5a122f37252fcae66002079757b90.

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -2,9 +2,11 @@ import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Button } from "react-bootstrap";
-import avatar from "../images/avatar.png";
 
 function Navi({ user }) {
+  const logout = () => {
+    window.open("http://localhost:8000/auth/logout", "_self");
+  };
   return (
     <>
       <Navbar
@@ -23,26 +25,25 @@ function Navi({ user }) {
           <Nav className="d-flex justify-content-between mr-4">
             {user ? (
               <>
-              <Link to="/profile">
-                <Navbar.Brand>
-                  <img
-                    src={avatar}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top user-avatar"
-                    alt=""
-                  />
-                  {user}
-                </Navbar.Brand>
-              </Link>
+                <Link to="/profile">
+                  <Navbar.Brand>
+                    <img
+                      src={user.photos[0].value}
+                      width="30"
+                      height="30"
+                      className="d-inline-block align-top user-avatar"
+                      alt=""
+                    />
+                    {user.name.givenName.toUpperCase()}
+                  </Navbar.Brand>
+                </Link>
 
-              <Link to="/login">
-                <Button variant="outline-info">LOGOUT</Button>
-              </Link>
-
+                <Link to="/login">
+                  <Button variant="outline-info" onClick={logout}>
+                    LOGOUT
+                  </Button>
+                </Link>
               </>
-
-              
             ) : (
               <Link to="/login">
                 <Button variant="outline-info">LOGIN</Button>
--- src/components/SocialLogin.js
+++ src/components/SocialLogin.js
@@ -1,28 +1,40 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
 import Apple from "../images/Apple.png";
 import Google from "../images/Google.png";
 import Outlook from "../images/Outlook.png";
 
 function SocialLogin() {
+  const google = () => {
+    window.open("http://localhost:8000/auth/google", "_self");
+  };
+
+  const outlook = () => {
+    window.open("http://localhost:8000/auth/google", "_self");
+  };
+
+  const apple = () => {
+    window.open("http://localhost:8000/auth/google", "_self");
+  };
+
   return (
     <>
-    <div className="loginButton google">
-              <img src={Google} alt="" className="icon" />
-              LogIn with Google
-            </div>
+      <div className="loginButton google" onClick={google}>
+        <img src={Google} alt="" className="icon" />
+        LogIn with Google
+      </div>
 
-            <div className="loginButton facebook">
-              <img src={Outlook} alt="" className="icon" />
-              LogIn with Outlook
-            </div>
+      <div className="loginButton outlook" onClick={outlook}>
+        <img src={Outlook} alt="" className="icon" />
+        LogIn with Outlook
+      </div>
 
-            <div className="loginButton github">
-              <img src={Apple} alt="" className="icon" />
-              LogIn with Apple
-            </div>
+      <div className="loginButton apple" onClick={apple}>
+        <img src={Apple} alt="" className="icon" />
+        LogIn with Apple
+      </div>
     </>
-  )
+  );
 }
 
-export default SocialLogin
+export default SocialLogin;
