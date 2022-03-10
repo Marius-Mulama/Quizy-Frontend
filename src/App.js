9039d3f7185630ddc63347ce14d18b6729c679c6
Marius <mulamamarius@gmail.com>
2022-03-11 01:01:22 +0300

    Revert "Added listeners to the different social logins --- Not yet completed--"
    This reverts commit ee4b34d8ece5a122f37252fcae66002079757b90.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navi from './components/Navi';
@@ -8,39 +7,13 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 
 function App() {
-  const [user,setUser] = useState(null)
-
-  useEffect(() => {
-    const getUser = () => {
-      fetch("http://localhost:8000/auth/login/success", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    getUser();
-  }, []);
-
- // console.log(user)
+  //const [user,setUser] = useState(null)
+  const user = "User";
 
   return (
     <BrowserRouter>
     <div className="App">
-      <Navi user={user} />
+      <Navi user={user}/>
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/login' element={user? <Navigate to="/"/>:<Login/>}/>
